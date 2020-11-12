ba96a76e1b364c2e8a424ff839c59f773a28d18e
Felipe TV <correo@mail.not>
2020-11-11 21:25:37 -0300

    add flask

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from registro import get
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    df = get()
+    return df.to_json()
 
 if __name__ == '__main__':
-    df = get()
-    print(df['rutDonante'])
+    app.run()
